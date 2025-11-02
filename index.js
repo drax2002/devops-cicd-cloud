ecaf6b58184899e4e4deadb11be36618f30c0112
Aman Lamsal <amanlamsal2002@gmail.com>
2025-11-02 11:31:33 -0500

    Add server listening log to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 app.get('/', (req, res) => {
   res.json({
