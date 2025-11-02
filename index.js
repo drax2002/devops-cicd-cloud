3045e54f845c882fe2fbd2695ac3320f27c5bfed
Aman Lamsal <amanlamsal2002@gmail.com>
2025-11-02 11:35:31 -0500

    Refactor port initialization for server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = process.env.PORT
+
 
 app.get('/', (req, res) => {
   res.json({
